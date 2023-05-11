064a8c485315671e454cb3eb7f51b3ac468162d6
Feliverse <haybar@gmail.com>
2023-05-11 18:29:51 -0400

    Method to show the 3 last posts per user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
+
+  def show_3_last
+    @user = User.find (params [:id]) # Encuentra el usuario por su id
+    @last_posts = @user.posts.last (3) # Obtiene los últimos 3 posts de ese usuario
+  end
 end
