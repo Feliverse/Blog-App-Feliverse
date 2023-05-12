454701698b4f2de662339e4291a5fce5b581d477
Feliverse <haybar@gmail.com>
2023-05-12 08:31:28 -0400

    Refactoring method to show the last 5 comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,25 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  # We use a class attribute to store the last 5 comments
+    @@last_5_comments = []
+  
+  # We use a callback after_save to update the attribute every time a comment is saved
 
-  def update_counter
-    post.increment!(:comment_counter)
+  after_save :update_last_5_comments
+  
+  # We use a callback after_save to update the attribute every time a comment is saved
+  def self.last_5_comments
+    @@last_5_comments
+  end
+  
+  # We define a private method to update the attribute with the newly saved comment
+  private
+  
+  def update_last_5_comments
+    # Add the comment to the beginning of the array
+    @@last_5_comments.unshift (self)
+    # We remove the last element of the array if it has more than 5 elements
+    @@last_5_comments.pop if @@last_5_comments.size > 5
   end
 end
