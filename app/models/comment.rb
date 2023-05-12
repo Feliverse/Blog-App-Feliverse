1d62c98553de519dc449f551afc167d6a8d29df2
Feliverse <haybar@gmail.com>
2023-05-12 09:32:31 -0400

    Fixing rubocop offenses

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, foreign_key: 'post_id'
 
-  def update_counter : do
+  def update_counter
     post.increment!(:comment_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,30 +3,31 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  # We use a class attribute to store the last 5 comments
-  @@last_5_comments = []
-  
-  # We use a callback after_save to update the attribute every time a comment is saved
+  # We use a class instance attribute to store the last 5 comments
+  @last_5_comments_result = []
 
-  after_save :update_last_5_comments
-  
   # We use a callback after_save to update the attribute every time a comment is saved
+  after_save :update_last_5_comments
+
+  # We use a method to initialize the attribute once
   def self.last_5_comments
-    @@last_5_comments
+    @last_5_comments_result ||= [] # rubocop:disable Naming/MemoizedInstanceVariableName
   end
-  
+
+  # We use attr_reader to define a reader method for the attribute
+  attr_reader :last_5_comments_result
+
   # We define a private method to update the attribute with the newly saved comment
   private
-  
+
   def update_last_5_comments
     # Add the comment to the beginning of the array
-    @@last_5_comments.unshift (self)
+    self.class.last_5_comments.unshift(self)
     # We remove the last element of the array if it has more than 5 elements
-    @@last_5_comments.pop if @@last_5_comments.size > 5
+    self.class.last_5_comments.pop if self.class.last_5_comments.size > 5
   end
-  
+
   def update_counter
     author.increment!(:post_counter)
   end
-
 end
