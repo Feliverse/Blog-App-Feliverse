5f5c25d9c34160fd9abbce1acdabc1bf279e62c8
Feliverse <haybar@gmail.com>
2023-05-24 16:48:45 -0400

    Fix Rubocop offenses

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   attribute :comment_counter, default: 0
   attribute :like_counter, default: 0
-  
+
   belongs_to :author, class_name: 'User'
   validates :title, presence: true, length: { maximum: 250 }
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
