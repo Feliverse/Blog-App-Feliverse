a1b1adc6136250bfebef299fbe188dfdce61cc8c
Feliverse <haybar@gmail.com>
2023-05-31 10:39:46 -0400

    Configure Test Factory_Bot_Rails

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -18,5 +18,32 @@ RSpec.describe 'Users' do
         expect(page).to have_content("Number of posts: #{user.post_counter}")
       end
     end
+
+    it 'Should redirect to the user profile page when clicking on the user name' do
+      click_link('Felipe')
+      expect(page).to have_current_path(user_path(User.first))
+    end
   end
+
+  describe 'User #show' do
+    before do
+      visit users_path(User.first.id)
+    end
+
+    it 'Should be able to view profile picture' do
+      expect(page).to have_css("img[src*='#{User.first.photo}']")
+    end
+
+    it 'Should be able to view the user\'s name' do
+      expect(page).to have_content(User.first.name)
+    end
+
+    it 'Should be able to see the num of posst the user has written' do
+      expect(page).to have_content("Number of posts: #{User.first.post_counter}")
+    end
+
+    it 'Should be able to see the user\'s bio' do
+      expect(page).to have_content(User.first.bio)
+    end
+end
 end
